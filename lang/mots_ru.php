455a31a9df16ad114193f90c742374e5c145a971
Denisb <denisb@a-working-class-hero-is-something-to.be>
2011-05-02 20:51:06 +0000

    langues (mots)

---
+++ lang/mots_ru.php
@@ -0,0 +1,86 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'avis_conseil_selection_mot_cle' => '<b>Важная группа:</b> Настоятельно рекомендуется выбрать ключевое слово в этой группе.',
+	'avis_doublon_mot_cle' => 'Слово с таким название уже существует. Вы уверенны, что хотите создать еще одно?',
+
+	// B
+	'bouton_checkbox_qui_attribue_mot_cle_administrateurs' => 'администраторы сайта',
+	'bouton_checkbox_qui_attribue_mot_cle_redacteurs' => 'редакторы',
+	'bouton_checkbox_qui_attribue_mot_cle_visiteurs' => 'посетители основной части сайта, когда они размещают сообщение на форуме.',
+
+	// I
+	'icone_creation_groupe_mots' => 'Создать новую группу ключевых слов',
+	'icone_creation_mots_cles' => 'Создать новое ключевое слово',
+	'icone_modif_groupe_mots' => 'Изменить эту группу ключевых слов',
+	'icone_modifier_mot' => 'Изменить ключевое слово',
+	'icone_mots_cles' => 'Кейворды',
+	'icone_supprimer_groupe_mots' => 'Удалить эту группу',
+	'icone_voir_tous_mots_cles' => 'Показать все ключевые слова',
+	'info_1_groupe_mots' => '1 groupe de mots', # NEW
+	'info_articles_lies_mot' => 'Статьи с этим ключевым словом',
+	'info_aucun_groupe_mots' => 'Aucun groupe de mots', # NEW
+	'info_aucun_mot_cle' => 'Aucun mot-clé', # NEW
+	'info_changer_nom_groupe' => 'Изменить название этой группы:',
+	'info_creation_mots_cles' => 'Создать и настроить ключевые слова сайта можно здесь',
+	'info_dans_groupe' => 'В группе:',
+	'info_delet_mots_cles' => 'Вы сделали запрос на удаление ключевого слова
+<b>@titre_mot@</b> (@type_mot@).Это ключевое слово связывается с 
+<b> @texte_lie </b> Вы должны подтвердить это решение: ',
+	'info_groupe_important' => 'Важная группа',
+	'info_modifier_mot' => 'Редактировать ключевое слово:',
+	'info_mots_cles' => 'Ключевые слова',
+	'info_mots_cles_association' => 'Ключевые слова в этой группе могут быть связаны с:',
+	'info_nb_groupe_mots' => '@nb@ groupes de mots', # NEW
+	'info_oui_suppression_mot_cle' => 'Я хочу удалить это ключевое слово.',
+	'info_question_mots_cles' => 'Хотите ли Вы использовать на Вашем сайте ключевые слова?',
+	'info_qui_attribue_mot_cle' => 'Ключевые слова в этой группе могут быть применены к:',
+	'info_remplacer_mot' => 'Remplacer "@titre@"', # NEW
+	'info_retirer_mot' => 'Удалить ключевое слово',
+	'info_retirer_mots' => 'Удалить все ключевые слова',
+	'info_rubriques_liees_mot' => 'Разделы, связанные с этим ключевым словом',
+	'info_selection_un_seul_mot_cle' => 'Вы можете выбрать <b> только одно ключевое слово </b> к этой группе.',
+	'info_supprimer_mot' => 'удалить это ключевое слово',
+	'info_titre_mot_cle' => 'Имя или название этого ключевого слова',
+	'info_un_mot' => 'Одно ключевое слово одновременно',
+	'item_ajout_mots_cles' => 'Разрешить дополнение форумов ключевыми словами',
+	'item_non_ajout_mots_cles' => 'Не разрешать дополнение форумов ключевыми словами',
+	'item_non_utiliser_config_groupe_mots_cles' => 'Не использовать дополнительную настройку групп ключевого слова',
+	'item_non_utiliser_mots_cles' => 'Не использовать ключевые слова',
+	'item_utiliser_config_groupe_mots_cles' => 'Использовать дополнительную настройку групп ключевых слов',
+	'item_utiliser_mots_cles' => 'Использовать ключевые слова',
+
+	// L
+	'lien_ajouter_mot' => 'Ajouter ce mot-clé', # NEW
+	'logo_groupe' => 'LOGO OF THIS GROUP', # NEW
+	'logo_mot_cle' => 'ЛОГОТИП КЛЮЧЕВОГО СЛОВА',
+
+	// T
+	'texte_config_groupe_mots_cles' => 'Вы желаете включить дополнительные настройки групп ключевых слов, 
+   определяя, например, что уникальное слово в 
+ группе   может быть отобрано, что группа является важной ...?',
+	'texte_mots_cles' => 'Ключевые слова позволяют Вам создавать тематические связи между Вашими 
+ статьями,  независимо от их размещения в разделе. Таким образом, Вы можете 
+  обогатить навигацию Вашего сайта или даже использовать эти 
+ свойства для настройки статей в своих шаблонах.',
+	'texte_mots_cles_dans_forum' => 'Хотите ли Вы разрешить посетителям выбирать ключевые слова в форумах основного сайта? (Предупреждение: эту опцию довольно сложно использовать правильно.)',
+	'texte_nouveau_mot' => 'Новое ключевое слово',
+	'titre_config_groupe_mots_cles' => 'Настройка групп ключевых слов',
+	'titre_gauche_mots_edit' => 'НОМЕР КЛЮЧЕВОГО СЛОВА:',
+	'titre_groupe_mots' => 'Groupe de mots-cl&eacute;s', # NEW
+	'titre_groupe_mots_numero' => 'GROUPE DE MOTS NUMÉRO :', # NEW
+	'titre_groupes_mots' => 'Groupes de mots-cl&eacute;s', # NEW
+	'titre_mots_cles_dans_forum' => 'Ключевые слова в форумах основного сайта',
+	'titre_mots_tous' => 'Ключевые слова',
+	'titre_nouveau_groupe' => 'Новая группа',
+	'titre_page_mots_tous' => 'Ключевые слова'
+);
+
+?>
---
+++ lang/mots_sk.php
@@ -0,0 +1,86 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://www.spip.net/trad-lang/
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'avis_conseil_selection_mot_cle' => '<b>Important group:</b> It is very strongly advised to select a keyword in this group.',
+	'avis_doublon_mot_cle' => 'A word with this title already exists. Are you sure you want to create another?',
+
+	// B
+	'bouton_checkbox_qui_attribue_mot_cle_administrateurs' => 'site administrators', # MODIF
+	'bouton_checkbox_qui_attribue_mot_cle_redacteurs' => 'editors',
+	'bouton_checkbox_qui_attribue_mot_cle_visiteurs' => 'visitors of the public site when they post a message on a forum.', # MODIF
+
+	// I
+	'icone_creation_groupe_mots' => 'Vytvoriť novú skupinu klúčových slov',
+	'icone_creation_mots_cles' => 'Vytvoriť nové kľúčové slovo',
+	'icone_modif_groupe_mots' => 'Upraviť túto skupinu kľúčových slov',
+	'icone_modifier_mot' => 'Edit this keyword',
+	'icone_mots_cles' => 'Kľúčové slová',
+	'icone_supprimer_groupe_mots' => 'Zmazať túto skupinu',
+	'icone_voir_tous_mots_cles' => 'Zobraziť všetky kľúčové slová',
+	'info_1_groupe_mots' => '1 groupe de mots', # NEW
+	'info_articles_lies_mot' => 'Articles associated with this keyword',
+	'info_aucun_groupe_mots' => 'Aucun groupe de mots', # NEW
+	'info_aucun_mot_cle' => 'Aucun mot-clé', # NEW
+	'info_changer_nom_groupe' => 'Change the name of this group:', # MODIF
+	'info_creation_mots_cles' => 'Create and configure the site\'s keywords here',
+	'info_dans_groupe' => 'In group:', # MODIF
+	'info_delet_mots_cles' => 'You requested the deletion of keyword
+<b>@titre_mot@</b> (@type_mot@). This keyword being linked to
+<b>@texte_lie@</b>you must confirm this decision:', # MODIF
+	'info_groupe_important' => 'Important group', # MODIF
+	'info_modifier_mot' => 'Edit the keyword:',
+	'info_mots_cles' => 'Keywords',
+	'info_mots_cles_association' => 'Keywords in this group can be associated with:',
+	'info_nb_groupe_mots' => '@nb@ groupes de mots', # NEW
+	'info_oui_suppression_mot_cle' => 'I want to delete this keyword permanently.',
+	'info_question_mots_cles' => 'Do you wish to use keywords in your site?',
+	'info_qui_attribue_mot_cle' => 'Keywords in this group can be assigned by:',
+	'info_remplacer_mot' => 'Remplacer "@titre@"', # NEW
+	'info_retirer_mot' => 'Zmazať toto kľúčové slovo',
+	'info_retirer_mots' => 'Zmazať všetké kľúčové slova',
+	'info_rubriques_liees_mot' => 'Sections associated with this keyword',
+	'info_selection_un_seul_mot_cle' => 'You can select <b>only one keyword</b> at a time in this group.',
+	'info_supprimer_mot' => 'delete this keyword',
+	'info_titre_mot_cle' => 'Name or title of this keyword',
+	'info_un_mot' => 'One keyword at a time',
+	'item_ajout_mots_cles' => 'Authorise the addition of keywords to forums',
+	'item_non_ajout_mots_cles' => 'Do not authorise the addition of keywords to forums',
+	'item_non_utiliser_config_groupe_mots_cles' => 'Nepoužívať rozšírené nastavenia skupín kľúčových slov',
+	'item_non_utiliser_mots_cles' => 'Do not use keywords',
+	'item_utiliser_config_groupe_mots_cles' => 'Používať rozšírené nastavenia skupín kľúčových slov',
+	'item_utiliser_mots_cles' => 'Používať kľúčové slová',
+
+	// L
+	'lien_ajouter_mot' => 'Ajouter ce mot-clé', # NEW
+	'logo_groupe' => 'LOGO DE CE GROUPE', # NEW
+	'logo_mot_cle' => 'KEYWORD\'S LOGO',
+
+	// T
+	'texte_config_groupe_mots_cles' => 'Do you wish to activate the advanced configuration of keyword groups,
+   by specifying, for instance that a unique word
+ per group could be selected, that a group is important...?', # MODIF
+	'texte_mots_cles' => 'Keywords allow you to create topical links between your articles
+  irrespective of their section location. That way you can
+  enrich the navigation of your site or even use these properties
+  to customise the articles in your templates.',
+	'texte_mots_cles_dans_forum' => 'Do you wish to allow visitors to select keywords in the public site forums? (Warning: this option is rather intricate to use properly.)', # MODIF
+	'texte_nouveau_mot' => 'New keyword', # MODIF
+	'titre_config_groupe_mots_cles' => 'Nastavenie skupín kľúčových slov',
+	'titre_gauche_mots_edit' => 'ČÍSLO SLOVA:',
+	'titre_groupe_mots' => 'Groupe de mots-cl&eacute;s', # NEW
+	'titre_groupe_mots_numero' => 'GROUPE DE MOTS NUMÉRO :', # NEW
+	'titre_groupes_mots' => 'Groupes de mots-cl&eacute;s', # NEW
+	'titre_mots_cles_dans_forum' => 'Keywords in the public site forums',
+	'titre_mots_tous' => 'Kľúčové slova',
+	'titre_nouveau_groupe' => 'Nová skupina',
+	'titre_page_mots_tous' => 'Kľúčové slova'
+);
+
+?>
