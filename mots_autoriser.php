4be3a9fb4eecba3e8d0191b69e4532b337e3c258
Cerdic <cedric@yterium.com>
2011-06-07 21:19:59 +0000

    http://core.spip.org/issues/2107 : c'est la table qui est inscrite dans le champ tables_liees, il faut donc passer par table_objet

--- mots_autoriser.php
+++ mots_autoriser.php
@@ -82,7 +82,7 @@ function autoriser_mot_creer_dist($faire, $type, $id, $qui, $opt) {
 	  AND $associer_objet = $opt['associer_objet']){
 		if (!preg_match(',^(\w+)\|[0-9]+$,',$associer_objet,$match))
 			return false;
-		$where = "tables_liees REGEXP '(^|,)".$match[1]."($|,)'";
+		$where = "tables_liees REGEXP '(^|,)".addslashes(table_objet($match[1]))."($|,)'";
 	}
 	// si pas de groupe de mot qui colle, pas le droit
 	if (!sql_countsel('spip_groupes_mots',$where))
