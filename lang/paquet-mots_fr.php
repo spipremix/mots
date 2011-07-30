3119d78082974a732a834677d3d7aeb7e0afb74f
eric <eric@smellup.net>
2011-07-30 14:52:13 +0000

    Début des tests du nouveau fichier paquet.xml. On ajoute pour chaque plugin du core : - le paquet.xml sous le nom _paquet.xml pour éviter une utilisation par défaut à la place de plugin.xml Il suffira de le renommer pour effectuer les tests. - les fichiers de langue nécessaire à la description et au slogan. - un fichier _paquet-migration qui contient les commandes svn pour migrer ou ajouter des fichiers. Ce fichier n'a plus d'autre intérêt que de lister les fichiers à renommer pour retrouver le fonctionnement des balises disparues : install, fonctions et options.

---
+++ lang/paquet-mots_fr.php
@@ -0,0 +1,18 @@
+<?php
+
+// Ceci est un fichier langue de SPIP -- This is a SPIP language file
+// Fichier produit par PlugOnet
+// Module: paquet-mots
+// Langue: fr
+// Date: 30-07-2011 15:01:37
+// Items: 2
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+// M
+	'mots_description' => 'Mots et Groupes de mots',
+	'mots_slogan' => 'Gestion des mots et groupes de mots dans SPIP',
+);
+?>
