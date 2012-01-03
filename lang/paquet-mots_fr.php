2563e833688675ead5120cbc6a48de39d2ea76b6
salvatore <salvatore@rezo.net>
2012-01-03 12:10:22 +0000

    langues (paquet-mots)

--- lang/paquet-mots_fr.php
+++ lang/paquet-mots_fr.php
@@ -1,18 +1,13 @@
 <?php
-
-// Ceci est un fichier langue de SPIP -- This is a SPIP language file
-// Fichier produit par PlugOnet
-// Module: paquet-mots
-// Langue: fr
-// Date: 30-07-2011 15:01:37
-// Items: 2
-
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// Fichier source, a modifier dans svn://zone.spip.org/spip-zone/_core_/plugins/mots/lang/
 if (!defined('_ECRIRE_INC_VERSION')) return;
 
 $GLOBALS[$GLOBALS['idx_lang']] = array(
 
-// M
+	// M
 	'mots_description' => 'Mots et Groupes de mots',
-	'mots_slogan' => 'Gestion des mots et groupes de mots dans SPIP',
+	'mots_slogan' => 'Gestion des mots et groupes de mots dans SPIP'
 );
+
 ?>
