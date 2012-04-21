5888350b056db6b3ac51a0ad6fc7393cdb001da6
Cerdic <cedric@yterium.com>
2012-04-21 09:25:14 +0000

    Ferme http://core.spip.org/issues/2655 : eviter une erreur SQL lorsque un mot reste associé à une table qui n'existe plus (plugin supprimé) corriger une coquille qui provoquait multiple recomptages identiques corriger un defaut CSS sur les pages de groupes

--- inc/mots.php
+++ inc/mots.php
@@ -17,10 +17,10 @@ include_spip('inc/actions');
 include_spip('base/abstract_sql');
 
 function filtre_objets_associes_mot_dist($id_mot,$id_groupe) {
-	static $occurences = array();
+	static $occurrences = array();
 
 	// calculer tous les liens du groupe d'un coup
-	if (!isset ($occurences[$id_groupe]))
+	if (!isset ($occurrences[$id_groupe]))
 		$occurrences[$id_groupe] = calculer_utilisations_mots($id_groupe);
 
 	$associes = array();
@@ -45,45 +45,47 @@ function filtre_objets_associes_mot_dist($id_mot,$id_groupe) {
 function calculer_utilisations_mots($id_groupe)
 {
 	$retour = array();
-	$objets = sql_allfetsel('DISTINCT objet', array('spip_mots_liens AS L', 'spip_mots AS M'), array('L.id_mot=M.id_mot', 'M.id_groupe='.$id_groupe));
+	$objets = sql_allfetsel('DISTINCT objet', array('spip_mots_liens AS L', 'spip_mots AS M'), array('L.id_mot=M.id_mot', 'M.id_groupe='.intval($id_groupe)));
 
 	foreach($objets as $o) {
 		$objet=$o['objet'];
 		$_id_objet = id_table_objet($objet);
 		$table_objet_sql = table_objet_sql($objet);
 		$infos = lister_tables_objets_sql($table_objet_sql);
-		// uniquement certains statut d'objet,
-		// et uniquement si la table dispose du champ statut.
-		$statuts = "";
-		if (isset($infos['field']['statut']) OR isset($infos['statut'][0]['champ'])) {
-			// on s'approche au mieux de la declaration de l'objet.
-			// il faudrait ameliorer ce point.
-			$c_statut = isset($infos['statut'][0]['champ']) ? $infos['statut'][0]['champ'] : 'statut';
-			
-			// bricoler les statuts d'apres la declaration de l'objet (champ previsu a defaut de mieux)
-			if (array_key_exists('previsu', $infos['statut'][0]) AND strlen($infos['statut'][0]['previsu'])>1) {
-				$str_statuts = $infos['statut'][0]['previsu'];
-				if ($GLOBALS['connect_statut'] !="0minirezo")
-					$str_statuts = str_replace('prepa','',$str_statuts);
-				$not = (substr($str_statuts, 0, 1) == '!'? 'NOT' : '');
-				$str_statuts = str_replace('!','',$str_statuts);
-				$Tstatuts = array_filter(explode(',', $str_statuts));
-				$statuts = " AND " . sql_in("O.$c_statut",  $Tstatuts, $not);
+		if (isset($infos['field']) AND $infos['field']){
+			// uniquement certains statut d'objet,
+			// et uniquement si la table dispose du champ statut.
+			$statuts = "";
+			if (isset($infos['field']['statut']) OR isset($infos['statut'][0]['champ'])) {
+				// on s'approche au mieux de la declaration de l'objet.
+				// il faudrait ameliorer ce point.
+				$c_statut = isset($infos['statut'][0]['champ']) ? $infos['statut'][0]['champ'] : 'statut';
+
+				// bricoler les statuts d'apres la declaration de l'objet (champ previsu a defaut de mieux)
+				if (array_key_exists('previsu', $infos['statut'][0]) AND strlen($infos['statut'][0]['previsu'])>1) {
+					$str_statuts = $infos['statut'][0]['previsu'];
+					if ($GLOBALS['connect_statut'] !="0minirezo")
+						$str_statuts = str_replace('prepa','',$str_statuts);
+					$not = (substr($str_statuts, 0, 1) == '!'? 'NOT' : '');
+					$str_statuts = str_replace('!','',$str_statuts);
+					$Tstatuts = array_filter(explode(',', $str_statuts));
+					$statuts = " AND " . sql_in("O.$c_statut",  $Tstatuts, $not);
+				}
+				// objets sans champ previsu ou avec un previsu == '!' (par ex les rubriques)
+				else
+					$statuts = " AND " . sql_in("O.$c_statut",  ($GLOBALS['connect_statut'] =="0minirezo")  ? array('prepa','prop','publie') : array('prop','publie'));
 			}
-			// objets sans champ previsu ou avec un previsu == '!' (par ex les rubriques)
-			else 		
-				$statuts = " AND " . sql_in("O.$c_statut",  ($GLOBALS['connect_statut'] =="0minirezo")  ? array('prepa','prop','publie') : array('prop','publie'));
-		}
-		$res = sql_allfetsel(
-			"COUNT(*) AS cnt, L.id_mot",
-			"spip_mots_liens AS L
-				LEFT JOIN spip_mots AS M ON L.id_mot=M.id_mot
-					AND L.objet=" . sql_quote($objet) . "
-				LEFT JOIN " . $table_objet_sql . " AS O ON L.id_objet=O.$_id_objet" ,
-			"M.id_groupe=$id_groupe$statuts",
-			"L.id_mot");
-	foreach($res as $row) {
-			$retour[$table_objet_sql][$row['id_mot']] = $row['cnt'];
+			$res = sql_allfetsel(
+				"COUNT(*) AS cnt, L.id_mot",
+				"spip_mots_liens AS L
+					LEFT JOIN spip_mots AS M ON L.id_mot=M.id_mot
+						AND L.objet=" . sql_quote($objet) . "
+					LEFT JOIN " . $table_objet_sql . " AS O ON L.id_objet=O.$_id_objet" ,
+				"M.id_groupe=$id_groupe$statuts",
+				"L.id_mot");
+			foreach($res as $row) {
+				$retour[$table_objet_sql][$row['id_mot']] = $row['cnt'];
+			}
 		}
 	}
 
