5d015059f5d9d617876286eaa0c159383c3ab06d
Matthieu Marcillaud <marcimat@rezo.net>
2012-08-21 08:26:36 +0000

    un @pipeline de plus

--- mots_pipelines.php
+++ mots_pipelines.php
@@ -20,6 +20,7 @@ if (!defined('_ECRIRE_INC_VERSION')) return;
 /**
  * Définir les meta de configuration liées aux mots
  *
+ * @pipeline configurer_liste_metas
  * @param array $metas
  *     Couples nom de la méta => valeur par défaut
  * @return array
