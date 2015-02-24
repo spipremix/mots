49c7de886a50f3ef612a20e1ee6ddc9063909b4d
sylvain <sylvain@oliver-fr.org>
2015-02-24 04:33:34 +0000

    [Salvatore] [source:_core_/plugins/mots/lang/ mots] Export depuis http://trad.spip.net de la langue oc_ni_mis

---
+++ lang/mots_oc_ni_mis.php
@@ -0,0 +1,53 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mots?lang_cible=oc_ni_mis
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'avis_conseil_selection_mot_cle' => '<b>Group empourtant:</b> es fouòrt acounsilhat de seleciounà un mot clau en aqueu group.',
+
+	// B
+	'bouton_checkbox_qui_attribue_mot_cle_administrateurs' => 'Lu aministratour dóu sit',
+	'bouton_checkbox_qui_attribue_mot_cle_redacteurs' => 'lu redatour',
+	'bouton_checkbox_qui_attribue_mot_cle_visiteurs' => 'lu visitaire dóu sit public coura pouòston un message en un forou.',
+
+	// I
+	'icone_creation_groupe_mots' => 'Creà un group de mot nouvèu',
+	'icone_creation_mots_cles' => 'Creà un mot clau nouvèu',
+	'icone_modif_groupe_mots' => 'Moudificà aqueu group de mot',
+	'icone_modifier_mot' => 'Moudificà aqueu mot clau',
+	'icone_mots_cles' => 'Mot clau',
+	'icone_supprimer_groupe_mots' => 'Suprimà aqueu group',
+	'icone_voir_groupe_mots' => 'Veire aqueu group de mot',
+	'icone_voir_tous_mots_cles' => 'Veire toui lu mot clau',
+	'info_1_groupe_mots' => '1 group de mot',
+	'info_aucun_groupe_mots' => 'Mìnga group de mot',
+	'info_aucun_mot_cle' => 'Mìnga mot clau',
+	'info_dans_groupe' => 'En lou group :',
+	'info_groupe_important' => 'Group empourtant',
+	'info_modifier_mot' => 'Moudificà lou mot clau',
+	'info_mots_cles' => 'Lu mot clau',
+	'info_nb_groupe_mots' => '@nb@ group de mot',
+	'info_supprimer_mot' => 'suprimà aqueu mot',
+	'item_non_utiliser_mots_cles' => 'Noun utilisà lu mot clau',
+	'item_utiliser_mots_cles' => 'Utilisà lu mot clau',
+
+	// L
+	'lien_ajouter_mot' => 'Ajustà aqueu mot clau',
+	'logo_groupe' => 'LOGOU D’AQUEU GROUP',
+	'logo_mot_cle' => 'LOGOU DÓU MOT CLAU',
+
+	// T
+	'texte_nouveau_mot' => 'Mot nouvèu',
+	'titre_groupe_mots' => 'Group de mot clau',
+	'titre_groupes_mots' => 'Group de mot clau',
+	'titre_mots_tous' => 'Lu mot clau',
+	'titre_nouveau_groupe' => 'Group nouvèu',
+	'titre_page_mots_tous' => 'Mot clau'
+);
+
+?>
