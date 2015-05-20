9303ba07daf51f9bb80d99977f92bc0b6d07a618
olly.olga <olly.olga@gmail.com>
2015-05-20 03:32:44 +0000

    [Salvatore] [source:_core_/plugins/mots/lang/ mots] Export depuis http://trad.spip.net de la langue uk

---
+++ lang/mots_uk.php
@@ -0,0 +1,84 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de http://trad.spip.net/tradlang_module/mots?lang_cible=uk
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) return;
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// A
+	'avis_conseil_selection_mot_cle' => '<b>Виділити групу:</b> дана група буде візуально більш помітною. Зручно використовувати для позначення важливих груп ключових слів.',
+	'avis_doublon_mot_cle' => 'Ключове слово з такою назвою вже існує. Ви впевнені, що бажаєте створити нове?',
+
+	// B
+	'bouton_checkbox_qui_attribue_mot_cle_administrateurs' => 'Администратори',
+	'bouton_checkbox_qui_attribue_mot_cle_redacteurs' => 'Автори',
+	'bouton_checkbox_qui_attribue_mot_cle_visiteurs' => 'відвідувачі загальнодоступного сайту, якщо вони відправляють повідомлення в форум.',
+
+	// C
+	'creer_et_associer_un_mot' => 'Створити і додати ключове слово',
+
+	// I
+	'icone_creation_groupe_mots' => 'Створити нову групу слів',
+	'icone_creation_mots_cles' => 'Нове ключове слово',
+	'icone_modif_groupe_mots' => 'Змінити групу ключових слів',
+	'icone_modifier_mot' => 'Змінити це ключове слово',
+	'icone_mots_cles' => 'Ключові слова',
+	'icone_supprimer_groupe_mots' => 'Видалити цю групу',
+	'icone_voir_groupe_mots' => 'Переглянути групу',
+	'icone_voir_tous_mots_cles' => 'Показати усі ключові слова',
+	'info_1_groupe_mots' => '1 група ключових слів',
+	'info_articles_lies_mot' => 'Статті з ключовим словом',
+	'info_aucun_groupe_mots' => 'Нема груп ключових слів',
+	'info_aucun_mot_cle' => 'Нема ключових слів',
+	'info_changer_nom_groupe' => 'Поміняти назву цієї групи :',
+	'info_creation_mots_cles' => 'Створіть та налаштуйте тут ключові слова сайту ',
+	'info_dans_groupe' => 'У групі:',
+	'info_delet_mots_cles' => 'Ви хочете видалити ключове слово <b>@titre_mot@</b> (@type_mot@). Воно пов’язано з  <b>@texte_lie@</b>. Підтвердіть видалення: ',
+	'info_groupe_important' => 'Важлива група',
+	'info_modifier_mot' => 'Змінити ключове слово:',
+	'info_mots_cles' => 'Ключові слова',
+	'info_mots_cles_association' => 'Ключові слова можна додавати до:',
+	'info_nb_groupe_mots' => '@nb@ груп ключових слів',
+	'info_oui_suppression_mot_cle' => 'Я хочу видалити ключове слово.',
+	'info_question_mots_cles' => 'Включити ключові слова на сайті?',
+	'info_qui_attribue_mot_cle' => 'Ключові слова до матеріалів можуть додавати:',
+	'info_remplacer_mot' => 'Замінити "@titre@"',
+	'info_retirer_mot' => 'Видалити ключове слово',
+	'info_retirer_mots' => 'Видалити всі ключові слова',
+	'info_rubriques_liees_mot' => 'Рубрики з ключовими словами',
+	'info_selection_un_seul_mot_cle' => 'До матеріалу можна додати лише <b> одне ключове слово</b> з цієї групи.',
+	'info_supprimer_mot' => 'видалити ключове слово',
+	'info_titre_mot_cle' => 'Назва ключового слова',
+	'info_un_mot' => 'Тільки одне ключове слово',
+	'item_ajout_mots_cles' => 'Включити додання ключових слів до коментарів на сайті',
+	'item_non_ajout_mots_cles' => 'Відключити додання ключових слів до коментарів на сайті',
+	'item_non_utiliser_config_groupe_mots_cles' => 'Відключити настройку груп ключових слів',
+	'item_non_utiliser_mots_cles' => 'Відключити ключові слова і групи ключових слів',
+	'item_utiliser_config_groupe_mots_cles' => 'Включити настройку груп ключових слів',
+	'item_utiliser_mots_cles' => 'Включити ключові слова і групи ключових слів',
+
+	// L
+	'lien_ajouter_mot' => 'Додати ключове слово',
+	'logo_groupe' => 'ЛОГОТИП ГРУПИ',
+	'logo_mot_cle' => 'ЛОГОТИП КЛЮЧОВОГО СЛОВА',
+
+	// T
+	'texte_config_groupe_mots_cles' => 'В налаштуваннях групи ви можете задавати, до яких типів матеріалів можна додавати ключові слова, виділяти важливі групи і ряд інших налаштувань.',
+	'texte_mots_cles' => 'Ключові слова - це можливість створити додаткову навігацію між матеріалами сайту. Також вони використовуються для створення додаткового функціоналу вашого сайту.',
+	'texte_mots_cles_dans_forum' => 'Дозволити відвідувачам додавати ключові слова до своїх коментарів на сайті?',
+	'texte_nouveau_mot' => 'Нове ключове слово',
+	'titre_config_groupe_mots_cles' => 'Налаштування груп ключових слів',
+	'titre_gauche_mots_edit' => 'НОМЕР КЛЮЧОВОГО СЛОВА:',
+	'titre_groupe_mots' => 'Групи ключових слів',
+	'titre_groupe_mots_numero' => 'НОМЕР ГРУПИ:',
+	'titre_groupes_mots' => 'Групи ключових слів',
+	'titre_mots_cles_dans_forum' => 'Ключові слова для коментарів',
+	'titre_mots_tous' => 'Ключові слова',
+	'titre_nouveau_groupe' => 'Нова група',
+	'titre_objets_lies_mot' => 'Матеріали з цим ключовим словом:',
+	'titre_page_mots_tous' => 'Ключові слова'
+);
+
+?>
