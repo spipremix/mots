7378769d041d2634061122032f66d22c1a1a4d5e
gpl <gpl@long-player.net>
2018-03-04 04:37:19 +0000

    [Salvatore] [source:_core_/plugins/mots/lang/ paquet-mots] Export depuis http://trad.spip.net de la langue eo

---
+++ lang/paquet-mots_eo.php
@@ -0,0 +1,15 @@
+<?php
+// This is a SPIP language file  --  Ceci est un fichier langue de SPIP
+// extrait automatiquement de https://trad.spip.net/tradlang_module/paquet-mots?lang_cible=eo
+// ** ne pas modifier le fichier **
+
+if (!defined('_ECRIRE_INC_VERSION')) {
+	return;
+}
+
+$GLOBALS[$GLOBALS['idx_lang']] = array(
+
+	// M
+	'mots_description' => 'Ŝlosilvortoj kaj grupoj de ŝlosilvortoj',
+	'mots_slogan' => 'Mastrumado de ŝlosilvortoj kaj grupoj de ŝlosilvortoj en SPIP'
+);
